4241c5f3e19234fb48d2c46792f4b5b832fe46f6
SUSMOR <159505308+SUSMOR@users.noreply.github.com>
2024-02-13 23:03:43 -0700

    Clientes.java

---
+++ Dominio/Clientes.java
@@ -0,0 +1,134 @@
+public class Clientes {
+    int id, edad;
+    String nombre, apellidoPaterno, apellidoMaterno, fehcadenacimiento, domicilio;
+
+    public Clientes() {
+    }
+
+    public Clientes(int id, int edad, String nombre, String apellidoPaterno, String apellidoMaterno, String fehcadenacimiento, String domicilio) {
+        this.id = id;
+        this.edad = edad;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.fehcadenacimiento = fehcadenacimiento;
+        this.domicilio = domicilio;
+    }
+
+    public Clientes(int edad, String nombre, String apellidoPaterno, String apellidoMaterno, String fehcadenacimiento, String domicilio) {
+        this.edad = edad;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.fehcadenacimiento = fehcadenacimiento;
+        this.domicilio = domicilio;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getFehcadenacimiento() {
+        return fehcadenacimiento;
+    }
+
+    public void setFehcadenacimiento(String fehcadenacimiento) {
+        this.fehcadenacimiento = fehcadenacimiento;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 41 * hash + this.id;
+        hash = 41 * hash + this.edad;
+        hash = 41 * hash + Objects.hashCode(this.nombre);
+        hash = 41 * hash + Objects.hashCode(this.apellidoPaterno);
+        hash = 41 * hash + Objects.hashCode(this.apellidoMaterno);
+        hash = 41 * hash + Objects.hashCode(this.fehcadenacimiento);
+        hash = 41 * hash + Objects.hashCode(this.domicilio);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Clientes other = (Clientes) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (this.edad != other.edad) {
+            return false;
+        }
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.apellidoPaterno, other.apellidoPaterno)) {
+            return false;
+        }
+        if (!Objects.equals(this.apellidoMaterno, other.apellidoMaterno)) {
+            return false;
+        }
+        if (!Objects.equals(this.fehcadenacimiento, other.fehcadenacimiento)) {
+            return false;
+        }
+        return Objects.equals(this.domicilio, other.domicilio);
+    }
+
+    @Override
+    public String toString() {
+        return "Clientes{" + "id=" + id + ", edad=" + edad + ", nombre=" + nombre + ", apellidoPaterno=" + apellidoPaterno + ", apellidoMaterno=" + apellidoMaterno + ", fehcadenacimiento=" + fehcadenacimiento + ", domicilio=" + domicilio + '}';
+    }    
+}
+
