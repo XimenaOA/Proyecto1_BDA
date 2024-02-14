f1d98116aef6787523e6900197e054bd10489d93
SUSMOR <159505308+SUSMOR@users.noreply.github.com>
2024-02-13 23:36:33 -0700

    interfaz_RetiroI.java

--- interfaces/interfaz_RetiroI.java
+++ interfaces/interfaz_RetiroI.java
@@ -174,6 +174,7 @@ public class Retiro extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>                        
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {                                         
