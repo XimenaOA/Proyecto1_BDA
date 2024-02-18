9b232f04aafb8a17fd893a054bc013875e531fb3
XimenaOA <159595764+XimenaOA@users.noreply.github.com>
2024-02-18 16:34:16 -0700

    script

--- Banco/Banco/BancoSQL.sql
+++ Banco/Banco/BancoSQL.sql
@@ -37,7 +37,7 @@ Folio int(10) unique key,
 estado varchar(20) check(estado in("Activo", "Espera", "Cancelado")),
 contrasena varchar(8),
 monto double,
-fecha date,
+fecha datetime,
 cuenta long,
 idCuenta int(10),
 foreign key (idCuenta) references Cuentas(idCuenta)
@@ -51,4 +51,18 @@ idCuenta int(10),
 foreign key(idCuenta) references Cuentas(idCuenta)
 );
 
-select * from Cuentas where idCuenta=8;
+INSERT INTO retiroSinCuentea (Folio, estado, contrasena, monto, fecha, cuenta, idCuenta) 
+VALUES (1, 'Activo', '12345678', 500.00, '2024-02-18', 1234567890, 1);
+
+INSERT INTO Cuentas (fechaApertura, monto, numeroDeCuenta, estado, idcliente) 
+VALUES ('2024-02-18', 1000.00, 1234567890, 'Activo', LAST_INSERT_ID());
+
+INSERT INTO Clientes (nombre, apellidopaterno, apellidomaterno, edad, fechaDeNacimiento, usr, contrasena) 
+VALUES ('Juan', 'Pérez', 'Gómez', '30', '1994-05-15', 'juanpgomez', 'contraseña123');
+
+
+
+select * from Cuentas;
+select * from Cuentas where idCuenta=1;
+select * from retirosincuentea where folio = 1;
+select * from retirosincuentea;
