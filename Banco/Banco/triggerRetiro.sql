e6801f5056e2c39cc79961b53b621376ff0f2cb4
XimenaOA <159595764+XimenaOA@users.noreply.github.com>
2024-02-18 16:47:42 -0700

    script

--- Banco/Banco/triggerRetiro.sql
+++ Banco/Banco/triggerRetiro.sql
@@ -3,8 +3,19 @@ DELIMITER //
 create trigger retiro_sin_cuenta_2
 before update on retirosincuentea
 for each row
-begin 
-update retirosincuentea set estado = "Activo";
--- values (old.folio, old.estado, old.contrasena, old.monto, old.fecha, old.idCuenta);
+begin
+declare fechat datetime;
+declare ahora datetime; 
+declare periodo datetime;
+set fechat = NEW.fecha;
+set ahora = now();
+set periodo = timestampdiff(minute, fechat, ahora);
+if periodo>10 then 
+if new.estado = "Espera" then
+set new.estado = "Cancelado";
+end if;
+end if;
 end //
 DELIMITER ;
+
+
